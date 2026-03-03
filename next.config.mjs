3d4854085649bb5b29516b15eb61fe74ae06e458
Harshavardhan G <harsha17jul4@gmail.com>
2026-03-03 14:18:55 +0530

    Use process.cwd() instead of __dirname in webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ import path from 'path';
 const nextConfig = {
   webpack: (config) => {
     // support the same '@' alias used in tsconfig.json
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    // __dirname is not defined in ESM; use process.cwd()
+    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
 
     config.resolve.alias.canvas = false;
     return config;
